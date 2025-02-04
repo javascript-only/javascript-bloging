435b45d67379813ae86dca0cf7a4e6c416df3e6b
Koras02 <wjdgh0727@gmail.com>
2025-02-05 04:43:07 +0900

    1.Data Type

---
+++ 1.string/1-2.string-var.js
@@ -0,0 +1,44 @@
+// var a;
+
+// console.log("a result" + a); // undefined
+
+// console.log ("b result"+ b); // undefined
+
+// var b;
+
+// console.log("c result" + c); // Uncaught ReferenceError: c is not defined
+
+// let x = 1;
+// console.log("x 값은" + x) // success x result 1
+
+ 
+
+// var thisArray = [];
+// if (!thisArray[0]) thisArray();
+
+// var a;
+
+// a + 2; // NaN
+
+
+
+// console.log(a);
+
+// var n = null;
+// console.log(n * 64) // 0
+
+// var a = 2;
+
+// console.log(a + 2);
+
+// if (true) {
+//     var x = 5;
+// }
+
+// console.log(x) // 5
+
+if (true) {
+    let x = 5;
+}
+
+console.log(y);  // Error
---
+++ 1.string/1-6.type-change.js
@@ -0,0 +1,18 @@
+// data type
+var answer = 42;
+
+answer = "Thanks for Watching"
+
+console.log(answer)
+
+// number + 
+
+x = "The answer is " + 42  // "The answer is 42"
+console.log(x);
+
+y = 42 + " is the answer" // "42 is the answer"
+console.log(y);
+
+
+"37" - 7; // 30
+"37" + 7 // "377";
